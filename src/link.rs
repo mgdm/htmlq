3551b5f8e023f8c85f79e7da7c49adf3db42609c
Michael Maclean <michael@no-surprises.co.uk>
2021-09-11 22:45:56 +0100

    Start of work on URL rewriting

---
+++ src/link.rs
@@ -0,0 +1,52 @@
+use kuchiki::NodeRef;
+use std::borrow::BorrowMut;
+use url::Url;
+
+fn rewrite_relative_urls(document: &NodeRef, base_url: &str) {
+    let base = Url::parse(base_url).unwrap();
+
+    for mut css_match in document
+        .select("a, area, link")
+        .expect("Failed to parse CSS selector while doing link rewriting")
+    {
+        let node = css_match.borrow_mut();
+        let mut attrs = node.attributes.borrow_mut();
+
+        if attrs.contains("href") {
+            let url = attrs.get("href").unwrap();
+            let new_url = base.join(url).unwrap().to_string();
+            attrs.insert("href", new_url);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use html5ever::tendril::TendrilSink;
+
+    use super::*;
+
+    fn make_doc(html: &mut String) -> NodeRef {
+        kuchiki::parse_html()
+            .from_utf8()
+            .read_from(&mut html.as_bytes())
+            .unwrap()
+    }
+
+    #[test]
+    fn it_works() {
+        let mut html =
+            "<html><head></head><body><a href=\"/foo/bar\">Hello</a></body></html>".to_string();
+        let expected_html =
+            "<html><head></head><body><a href=\"https://mgdm.net/foo/bar\">Hello</a></body></html>";
+
+        let doc = make_doc(&mut html);
+        rewrite_relative_urls(&doc, "https://mgdm.net");
+
+        let mut content: Vec<u8> = Vec::new();
+        doc.serialize(&mut content).unwrap();
+        let result = std::str::from_utf8(&content).unwrap();
+
+        assert_eq!(expected_html, result);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate kuchiki;
 #[macro_use]
 extern crate lazy_static;
 
+mod link;
 mod pretty_print;
 
 use clap::{App, Arg, ArgMatches};
@@ -19,6 +20,7 @@ struct Config {
     input_path: String,
     output_path: String,
     selector: String,
+    base: Option<String>,
     text_only: bool,
     ignore_whitespace: bool,
     pretty_print: bool,
@@ -37,9 +39,15 @@ impl Config {
             None => String::from("html"),
         };
 
+        let base: Option<String> = match matches.value_of("base") {
+            Some(val) => Some(val.to_owned()),
+            _ => None,
+        };
+
         Some(Config {
             input_path: String::from(matches.value_of("filename").unwrap_or("-")),
             output_path: String::from(matches.value_of("output").unwrap_or("-")),
+            base: base,
             text_only: matches.is_present("text_only"),
             ignore_whitespace: matches.is_present("ignore_whitespace"),
             pretty_print: matches.is_present("pretty_print"),
@@ -55,6 +63,7 @@ impl Default for Config {
             input_path: "-".to_string(),
             output_path: "-".to_string(),
             selector: "html".to_string(),
+            base: None,
             ignore_whitespace: true,
             pretty_print: true,
             text_only: false,
@@ -137,6 +146,13 @@ fn get_config<'a, 'b>() -> App<'a, 'b> {
                 .long("attribute")
                 .takes_value(true)
                 .help("Only return this attribute (if present) from selected elements"),
+        )
+        .arg(
+            Arg::with_name("base")
+                .short("b")
+                .long("base")
+                .takes_value(true)
+                .help("Use this URL as the base for links"),
         )
         .arg(
             Arg::with_name("selector")
