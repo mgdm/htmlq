cb322384b16db31a132f582e22d7268f16010fc4
zdebra <zd.brabec@gmail.com>
2022-03-12 18:31:12 +0100

    find by id

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,16 +1,33 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
 
-#[test]
-fn happy_path3() {
-    let input = "<html><head></head><body><div class=\"hi\"><a href=\"/foo/bar\">Hello</a></div></body></html>";
-    let expected_out = "<div class=\"hi\"><a href=\"/foo/bar\">Hello</a></div>\n";
-
-    Command::cargo_bin("htmlq")
-        .unwrap()
-        .arg(".hi")
-        .write_stdin(input)
-        .assert()
-        .success()
-        .stdout(predicate::str::diff(expected_out));
+macro_rules! cmd_success_tests {
+    ($($name:ident: $value:expr,)*) => {
+    $(
+        #[test]
+        fn $name(){
+            let (stdin, arg, expected) = $value;
+            Command::cargo_bin("htmlq")
+                .unwrap()
+                .arg(arg)
+                .write_stdin(stdin)
+                .assert()
+                .success()
+                .stdout(predicate::str::diff(expected));
+        }
+    )*
+    }
 }
+
+cmd_success_tests!(
+    find_by_class: (
+        "<html><head></head><body><div class=\"hi\"><a href=\"/foo/bar\">Hello</a></div></body></html>",
+        ".hi",
+        "<div class=\"hi\"><a href=\"/foo/bar\">Hello</a></div>\n"
+    ),
+    find_by_id: (
+        "<html><head></head><body><div id=\"my-id\"><a href=\"/foo/bar\">Hello</a></div></body></html>",
+        "#my-id",
+        "<div id=\"my-id\"><a href=\"/foo/bar\">Hello</a></div>\n"
+    ),
+);
