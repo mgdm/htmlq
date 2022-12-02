19c7f3f7bfb6d02460134df7d8dc68889247c2b3
zamu-flowerpot <106848396+zamu-flowerpot@users.noreply.github.com>
2022-12-02 00:41:58 -0500

    refactor: URL Rewriting, Writing to Output
    I ran into an issue where a website had a malformed `href` in a `link` element so URL rewriting failed. I tried to just use `-r` to remove the nodes from the selected set, but htmlq still panicked.
    
    I decided to look at the code to fix it and found  some improvements along the way.
    
    ---
    
    Changes
    
    * Refactored code to only rewrite URLs when they are part of the selected elements
        * As part of this refactor, we also only remove nodes  when they are part of the selected nodes.
        * Node Removal CSS Selectors were joined into a single selector as they are semantically the same.
            * eg. looping over `a` and `link` is the same as just using the single selector `a, link`
        * Refactored node removal to happen before URL rewrites.
        * Refactored `rewrite_relative_urls` with `rewrite_relative_url`
        * Removes leading forward slashes ('/') from `href`s in `a`, `link`, or `area` elems and doesn't join them if they have 4 or more leading forward slashes.
    
    * Replaced `output.write_all` calls with `writeln!` calls.
        * This should reduce string allocations to just include newlines.
        * It also only operates on nodes we've already selected instead of the whole tree.
    
    * Also replaced as many `.unwrap()` calls within the code (left the ones in the test since they should be fine.
        * Mainly changed over to `.expects()`, `.unwrap_or_default()`, or `.unwrap_or_else()` calls
        * Some minor Rust changes to use newer constructs like [`let-else`](https://rust-lang.github.io/rfcs/3137-let-else.html)

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,11 @@ mod pretty_print;
 
 use clap::{App, Arg, ArgMatches};
 use kuchiki::traits::*;
-use kuchiki::ElementData;
-use kuchiki::NodeDataRef;
 use kuchiki::NodeRef;
+use std::borrow::BorrowMut;
 use std::error::Error;
 use std::fs::File;
 use std::io;
-use std::process;
 use std::str;
 use url::Url;
 
@@ -87,7 +85,7 @@ fn select_attributes(node: &NodeRef, attributes: &[String], output: &mut dyn io:
         for attr in attributes {
             if let Ok(elem_atts) = as_element.attributes.try_borrow() {
                 if let Some(val) = elem_atts.get(attr.as_str()) {
-                    output.write_all(format!("{}\n", val).as_ref()).unwrap();
+                    writeln!(output, "{}", val).ok();
                 }
             }
         }
@@ -195,81 +193,75 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut input: Box<dyn io::Read> = match config.input_path.as_ref() {
         "-" => Box::new(std::io::stdin()),
-        f => Box::new(File::open(f).unwrap()),
+        f => Box::new(File::open(f).expect("should have opened input file")),
     };
 
     let stdout = std::io::stdout();
     let mut output: Box<dyn io::Write> = match config.output_path.as_ref() {
         "-" => Box::new(stdout.lock()),
-        f => Box::new(File::create(f).unwrap()),
+        f => Box::new(File::create(f).expect("should have created output file")),
     };
-
-    let mut base: Option<Url> = None;
-    if let Some(b) = config.base {
-        let u = Url::parse(&b);
-
-        if let Err(e) = u {
-            eprintln!("Failed to parse the provided base URL: {}", e);
-            process::exit(1);
-        }
-
-        base = Some(u.unwrap());
-    }
 
     let document = kuchiki::parse_html().from_utf8().read_from(&mut input)?;
 
-    if config.detect_base {
-        if let Some(b) = link::detect_base(&document) {
-            base = Some(b)
-        }
-    }
+    let base: Option<Url> = match (&config.base, &config.detect_base) {
+        (Some(base), true) => link::detect_base(&document).or(Url::parse(&base).ok()),
+        (Some(base), false) => Url::parse(&base).ok(),
+        (None, true) => link::detect_base(&document),
+        _ => None,
+    };
 
-    if let Some(base) = base {
-        link::rewrite_relative_urls(&document, &base);
-    }
+    let remove_node_selector = match config.remove_nodes {
+        Some(ref remove_node_selectors) => remove_node_selectors.join(","),
+        None => Default::default(),
+    };
 
-    if config.remove_nodes.is_some() {
-        for selector in config.remove_nodes.unwrap() {
-            let node_refs: Vec<NodeDataRef<ElementData>> = document
-                .select(&selector)
-                .expect("Failed to parse CSS selector")
-                .collect();
-
-            // For some reason I can't just iterate over this directly
-            // I need to collect each node and then detach them separately
-            for node_ref in node_refs {
-                node_ref.as_node().detach();
-            }
-        }
-    }
-
-    for css_match in document
+    document
         .select(&config.selector)
         .expect("Failed to parse CSS selector")
-    {
-        let node = css_match.as_node();
+        .filter(|noderef| {
+            if let Ok(mut node) = noderef.as_node().select_first(&remove_node_selector) {
+                node.borrow_mut().as_node().detach();
+                false
+            } else {
+                true
+            }
+        })
+        .map(|node| {
+            if let Some(base) = &base {
+                link::rewrite_relative_url(node.as_node(), &base)
+            }
+            node
+        })
+        .for_each(|matched_noderef| {
+            let node = matched_noderef.as_node();
 
-        if let Some(attributes) = &config.attributes {
-            select_attributes(node, attributes, &mut output);
-            continue;
-        }
+            if let Some(attributes) = &config.attributes {
+                select_attributes(node, attributes, &mut output);
+                return;
+            }
 
-        if config.text_only {
-            let content = serialize_text(node, config.ignore_whitespace);
-            output.write_all(format!("{}\n", content).as_ref())?;
-            continue;
-        }
+            if config.text_only {
+                // let content = serialize_text(node, config.ignore_whitespace);
+                // output.write_all(format!("{}\n", content).as_ref()).ok();
+                writeln!(output, "{}", serialize_text(node, config.ignore_whitespace)).ok();
+                return;
+            }
 
-        if config.pretty_print {
-            let content = pretty_print::pretty_print(node);
-            output.write_all(content.as_ref())?;
-            continue;
-        }
+            if config.pretty_print {
+                // let content = pretty_print::pretty_print(node);
+                // output.write_all(content.as_ref()).ok();
+                writeln!(output, "{}", pretty_print::pretty_print(node)).ok();
+                return;
+            }
 
-        let mut content: Vec<u8> = Vec::new();
-        node.serialize(&mut content)?;
-        output.write_all(format!("{}\n", str::from_utf8(&content)?).as_ref())?;
-    }
+            writeln!(output, "{}", node.to_string()).ok();
+            // let mut content: Vec<u8> = Vec::new();
+            // let Ok(_) = node.serialize(&mut content) else {
+            //     return
+            // };
+            // output.write_all(format!("{}\n", content).as_ref()).ok();
+        });
 
     Ok(())
 }
